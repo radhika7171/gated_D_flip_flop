54e9a567d9c83df6be1d93026416520986f1c49f
radhika7171 <73801260+radhika7171@users.noreply.github.com>
2023-02-25 18:00:56 -0500

    Add files via upload

---
+++ gated_d_ff.srcs/sources_1/new/d_ff.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+
+module dff(
+    input clk, din,
+    output reg dout
+    );
+always@(posedge clk)
+begin
+dout <= din;
+end  
+endmodule
+ 
+module top(clk, a, din, dout);
+input a,din;
+output  dout;
+(* gated_clock = "yes" *)input clk;  
+wire t1;
+ 
+assign t1 = a & clk;
+dff d1 (t1,din,dout);
+ 
+ 
+endmodule
